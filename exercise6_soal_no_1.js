34c447aa4236242fac0a109de2ea521683759c80
fadhdwisamns <49179388+fadhdwisamns@users.noreply.github.com>
2019-08-21 14:02:18 +0700

    Add files via upload

---
+++ exercise6_soal_no_1.js
@@ -0,0 +1,16 @@
+console.log('Looping Pertama')
+
+var flag = 2;
+while(flag <= 21) {
+  console.log(flag + ' I love coding')
+  flag = flag +2;
+}  
+
+
+console.log('Looping Kedua')
+
+var tes = 20;
+while(tes > 0) {
+  console.log(tes + ' I will become fullstack developer')
+  tes-=2;
+} 
